4912fb7ab4ce291a936f1c11ba74de917fbddd3a
LuPeBreak <lfbmrj15@gmail.com>
2023-04-10 07:46:19 -0300

    Criado layout Default - salvando para continuar no trabalho nas horas vagas

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,4 +1,5 @@
 import {Route, Routes} from 'react-router-dom'
+import { DefaultLayout } from './layouts/DefaultLayout'
 import { Checkout } from './pages/Checkout'
 import { Home } from './pages/Home'
 import { Sucess } from './pages/Sucess'
@@ -6,7 +7,7 @@ import { Sucess } from './pages/Sucess'
 export function Router(){
   return(
     <Routes>
-      <Route path='/'>
+      <Route path='/' Component={DefaultLayout}>
         <Route path='/' Component={Home}/>
         <Route path='/checkout' Component={Checkout}/>
         <Route path='/sucess' Component={Sucess}/>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,7 +3,6 @@ import { Header } from "../components/Header";
 export function Home(){
   return (
     <>
-      <Header/>
       <h1>Home</h1>
     </>
   )
